a240a10f47e1362f0338f6593e212df0f36d8934
Magnus Nordahl <54741107+magnusnor@users.noreply.github.com>
2024-05-17 20:58:09 +0200

    Make MSCN model estimate cardinality MySQL query

--- ml/predict.py
+++ ml/predict.py
@@ -4,14 +4,51 @@ import os
 
 import torch
 from torch.autograd import Variable
+from torch.utils.data import dataset
 from torch.utils.data import DataLoader
 
 from mscn.util import *
-from mscn.data import get_train_datasets, load_data, load_data_with_sampling, make_dataset
+from mscn.data import get_train_datasets, load_data, load_data_with_sampling, make_dataset, make_tensors
 from mscn.model import SetConv
 
+from dotenv import load_dotenv
+load_dotenv()
 
-def predict_dataset(model, data_loader, cuda):
+from logging_config import get_logger
+
+logger = get_logger(__name__)
+
+MODEL_CHECKPOINTS_PATH = os.environ["MODEL_CHECKPOINTS_PATH"]
+
+def run_inference_on_query(model, data_loader, cuda):
+    """
+    Run inference on a single query.
+    Returns the prediction.
+    """
+    preds = []
+    t_total = 0.
+
+    model.eval()
+    for batch_idx, data_batch in enumerate(data_loader):
+        tables, predicates, joins, table_masks, predicate_masks, join_masks = data_batch
+
+        if cuda:
+            tables, predicates, joins = tables.cuda(), predicates.cuda(), joins.cuda()
+            table_masks, predicate_masks, join_masks = table_masks.cuda(), predicate_masks.cuda(), join_masks.cuda()
+        tables, predicates, joins = Variable(tables), Variable(predicates), Variable(joins)
+        table_masks, predicate_masks, join_masks = Variable(table_masks), Variable(predicate_masks), Variable(join_masks)
+
+        t = time.time()
+
+        outputs = model(tables, predicates, joins, table_masks, predicate_masks, join_masks)
+        t_total += time.time() - t
+
+        for i in range(outputs.data.shape[0]):
+            preds.append(outputs.data[i])
+
+    return preds, t_total
+
+def run_inference_on_dataset(model, data_loader, cuda):
     """
     Run inference on each query in a given dataset.
     Returns a list of all predictions.
@@ -26,10 +63,8 @@ def predict_dataset(model, data_loader, cuda):
         if cuda:
             tables, predicates, joins, targets = tables.cuda(), predicates.cuda(), joins.cuda(), targets.cuda()
             table_masks, predicate_masks, join_masks = table_masks.cuda(), predicate_masks.cuda(), join_masks.cuda()
-        tables, predicates, joins, targets = Variable(tables), Variable(predicates), Variable(joins), Variable(
-            targets)
-        table_masks, predicate_masks, join_masks = Variable(table_masks), Variable(predicate_masks), Variable(
-            join_masks)
+        tables, predicates, joins, targets = Variable(tables), Variable(predicates), Variable(joins), Variable(targets)
+        table_masks, predicate_masks, join_masks = Variable(table_masks), Variable(predicate_masks), Variable(join_masks)
 
         t = time.time()
 
@@ -46,7 +81,6 @@ def predict_workload(workload_name, num_materialized_samples, num_queries, batch
     Predicts the cardinality for each query in a given workload.
     Writes the results to a CSV file.
     """
-    # file_name_column_min_max_vals = "data/column_min_max_vals.csv"
 
     print(f"Number of materialized base table samples: {num_materialized_samples}")
 
@@ -97,7 +131,7 @@ def predict_workload(workload_name, num_materialized_samples, num_queries, batch
             test_data = make_dataset(tables_test, predicates_test, joins_test, labels_test, max_num_joins, max_num_predicates)
 
         test_data_loader = DataLoader(test_data, batch_size=batch_size)
-        preds_test, t_total = predict_dataset(model, test_data_loader, cuda)
+        preds_test, t_total = run_inference_on_dataset(model, test_data_loader, cuda)
         print("Prediction time per test sample: {}".format(t_total / len(labels_test) * 1000))
 
         # Unnormalize
@@ -117,25 +151,51 @@ def predict_workload(workload_name, num_materialized_samples, num_queries, batch
             for i in range(len(preds_test_unnorm)):
                 f.write(str(preds_test_unnorm[i]) + "," + label[i] + "\n")
 
-def predict_query(num_materialized_samples):
+def predict_query(query, batch_size=256, cuda=False):
     """
     Run inference on a single query.
     Returns the cardinality estimate for the query.
     """
-    #TODO: Get inputs from MySQL and run inference.
 
-    if (num_materialized_samples > 0):
-        checkpoint_path = "checkpoints/model_sampling.pth"
-    else:
-        checkpoint_path = "checkpoints/model.pth"
+    checkpoint_path = f"{MODEL_CHECKPOINTS_PATH}/model.pth"
     checkpoint = torch.load(checkpoint_path)
 
-    model = SetConv(checkpoint["sample_feats"], checkpoint["predicate_feats"], checkpoint["join_feats"], hid_units=256)
-    model.load_state_dict(checkpoint["model_state_dict2"])
+    model = SetConv(checkpoint["table_feats"], checkpoint["predicate_feats"], checkpoint["join_feats"], hid_units=256)
+    model.load_state_dict(checkpoint["model_state_dict"])
     model.eval()
 
-    print(f"Loaded checkpoint from {os.path.abspath(checkpoint_path)}")
-    return
+    logger.info(f"Loaded checkpoint from {os.path.abspath(checkpoint_path)}")
+
+    column_min_max_vals = checkpoint["column_min_max_vals"]
+    max_num_joins = checkpoint["max_num_joins"]
+    max_num_predicates = checkpoint["max_num_predicates"]
+    min_val = checkpoint["min_val"]
+    max_val = checkpoint["max_val"]
+    table2vec, column2vec, op2vec, join2vec = checkpoint["dicts"]
+
+    joins = [query["joins"]]
+    predicates = [query["predicates"]]
+    tables = [query["tables"]]
+
+    tables_test = encode_tables(tables, table2vec)    
+    predicates_test, joins_test = encode_data(predicates, joins, column_min_max_vals, column2vec, op2vec, join2vec)
+
+    table_tensors, predicate_tensors, join_tensors, table_masks, predicate_masks, join_masks = make_tensors(tables_test, predicates_test, joins_test, max_num_joins, max_num_predicates)
+    
+    query_representation = dataset.TensorDataset(table_tensors, predicate_tensors, join_tensors, table_masks, predicate_masks, join_masks)
+
+    data_loader = DataLoader(query_representation, batch_size=batch_size)
+
+    prediction_norm, t_total = run_inference_on_query(model, data_loader, cuda=cuda)
+    cardinality_estimate = unnormalize_labels(prediction_norm, min_val, max_val)[0]
+
+    t_total_ms = t_total*1000
+    
+    logger.info(f"Inference time (ms): {t_total_ms}")
+    logger.info(f"Cardinality estimate: {cardinality_estimate}")
+
+    return cardinality_estimate
+
 
 def main():
     parser = argparse.ArgumentParser()
--- ml/server.py
+++ ml/server.py
@@ -5,13 +5,6 @@ import json
 from predict import predict_query
 from logging_config import get_logger
 
-# def uint64_to_bitmap(value):
-#     # Create a bitmap list with 64 bits (0 or 1) representing the value
-#     return [(value >> i) & 1 for i in reversed(range(64))]
-
-# def display_bitmap(bitmap):
-#     print('[' + ', '.join(map(str, bitmap)) + ']')
-
 logger = get_logger(__name__)
 
 SOCKET_PATH = "/tmp/mysqld-ml.sock"
@@ -22,37 +15,6 @@ if os.path.exists(SOCKET_PATH):
 server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM, 0)
 server.bind(SOCKET_PATH)
 server.listen(1)
-
-# while True:
-
-#     logger.info('Server is listening for incoming connections...')
-#     connection, client_address = server.accept()
-#     logger.info(f'Connection from: {str(connection.getsockname())}')
-
-    # while True:
-    #     data = connection.recv(2048)
-    #     if not data:
-    #         break
-    #     decoded_data = data.decode()
-    #     logger.info(f"Decoded data: {decoded_data}, Type: {type(decoded_data)}")
-
-    #     # bitmap = uint64_to_bitmap(int(float(decoded_data)))
-    #     # logging.info(f"Bitmap: {bitmap}")
-    #     # connection.sendall(str(bitmap).encode())
-    #     # display_bitmap(bitmap)
-
-    #     pattern = r'\(\s*(.*?)\s*=\s*(.*?)\s*\)'
-    #     result = re.sub(pattern, r'\1=\2', decoded_data)
-
-    #     query = dict()
-    #     query["tables"] = [result]
-    #     query["joins"] = [result]
-    #     query["predicates"] = [result]
-
-    #     logger.info(query)
-        
-    #     cardinality = predict_query(query)
-    #     connection.sendall(str(cardinality).encode())
 
 while True:
     logger.info('Server is listening for incoming connections...')
@@ -71,12 +33,6 @@ while True:
                 for msg in messages[:-1]:
                     query_rep = json.loads(msg)
                     logger.info(f"Received query representation from MySQL: {query_rep}")
-                    # query = {
-                    #     'tables': [query_rep["tables"]],
-                    #     'joins': [query_rep["joins"]],
-                    #     'predicates': [query_rep["predicates"]]
-                    # }
-                    # logger.info(f"Query representation: {query}")
                     cardinality = predict_query(query_rep)
                     connection.sendall(str(cardinality).encode())
                 data_buffer = messages[-1]
